a1f6b732c8abd41b08a09649ec5eecd6d6b83761
Dhruv Kulshreshtha <93292796+dhruv-kulsh@users.noreply.github.com>
2024-03-08 05:48:51 +0530

    Time: 24 ms (5.08%) | Memory: 19.3 MB (5.07%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -10,23 +10,24 @@ class Solution {
         }
         return arr;
     }
+
+    bool palindromecheck(vector<int>arr, int s, int e){
+        if( s > e)
+        return true;
+        if(arr[s] != arr[e]){
+                return false;
+            }
+
+            return palindromecheck(arr, s+1, e-1);
+    }
+
 public:
     bool isPalindrome(int x) {
         if(x < 0)
         return false;
         vector<int>arr = getarr(x);
-        for(auto i : arr){
-            cout<<i<<endl;
-        }
         int s = 0;
         int e = arr.size()-1;
-        while(s < e){
-            if(arr[s] != arr[e]){
-                return false;
-            }
-            s++;
-            e--;
-        }
-        return true;
+       return palindromecheck(arr, s, e);
     }
 };
