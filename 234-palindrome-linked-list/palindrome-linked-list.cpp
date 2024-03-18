6a847df244ab0bdb28559a4849d758ec29af4c54
Dhruv Kulshreshtha <93292796+dhruv-kulsh@users.noreply.github.com>
2024-03-18 18:43:15 +0530

    Time: 264 ms (5.12%) | Memory: 116.5 MB (82.27%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -9,24 +9,51 @@
  * };
  */
 class Solution {
-public:
-    bool isPalindrome(ListNode* head) {
-        if(head == NULL) return true;
-        ListNode *temp =  head;
-        vector<int>arr;
-        while(temp!= NULL){
-            int data = temp->val;
-            arr.push_back(data);
-            temp = temp->next;
+    ListNode * findMiddle(ListNode *head){
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while(fast != NULL){
+            slow = slow->next;
+            fast = fast->next;
+            if(fast != NULL)
+            fast = fast->next;
         }
-        int s = 0;
-        int e = arr.size()-1;
-        while(s < e){
-            if(arr[s] != arr[e]) return false;
-            s++;
-            e--;
+        return slow;
+    }
+
+    ListNode *reverse(ListNode *head){
+        ListNode *prev = NULL;
+        ListNode *curr = head;
+        ListNode *forward = NULL;
+
+        while(curr != NULL){
+            forward = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = forward;
+        }
+        return prev;
+    }
+    bool check(ListNode* head1, ListNode* head2){
+        while(head2 != NULL){
+            if(head1->val != head2->val) return false;
+            head1 = head1->next;
+            head2 = head2->next;
         }
         return true;
-
+    }
+    void print(ListNode* head){
+        while(head != NULL){
+            cout<<head->val<<" ";
+            head = head->next;
+        }
+    }
+public:
+    bool isPalindrome(ListNode* head) {
+        if(head == NULL || head->next == NULL) return true;
+        ListNode *middle = findMiddle(head);
+        print(middle);
+        ListNode *head2 = reverse(middle);
+        return check(head, head2);
     }
 };
